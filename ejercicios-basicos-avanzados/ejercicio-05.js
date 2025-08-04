fa1f48772181fc203549f45b01fc0390608842e3
RosarioGPerea <rosario.g.perea@gmail.com>
2025-08-04 14:37:44 +0200

    V_01

---
+++ ejercicios-basicos-avanzados/ejercicio-05.js
@@ -0,0 +1,36 @@
+En base a este código, cambia todos los */COMPLETAR /* por la condición correspondiente para que se lancen todos los console.log correctamente.
+
+Tienes como referencia el ejemplo del primer if.
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (/* COMPLETAR */) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (/* COMPLETAR */) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (/* COMPLETAR */) {
+  console.log("number3 es distinto number1");
+}
+
+if (/* COMPLETAR */) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (/* COMPLETAR */) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (/* COMPLETAR */) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
