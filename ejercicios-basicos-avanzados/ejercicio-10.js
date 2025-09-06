f27d8e725b8a72ceba246fe199b26f895a26b601
RosarioGPerea <rosario.g.perea@gmail.com>
2025-09-06 11:54:49 +0200

    V_02

--- ejercicios-basicos-avanzados/ejercicio-10.js
+++ ejercicios-basicos-avanzados/ejercicio-10.js
@@ -1,8 +1,16 @@
-Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
+/* Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
 
-Puedes usar este array para probarla:
+Puedes usar este array para probarla: */
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
+
 function average(numberList) {
-  // Completar
+  let sumando = 0;
+  for (let i = 0; i < numberList.length; i++) {
+    sumando += numberList[i];
+  }
+  
+  return sumando / numberList.length;
 }
+
+console.log(average(numbers));
