c5b1d1d3eea6ef3a841c0225d34b488ba0e6237c
RosarioGPerea <rosario.g.perea@gmail.com>
2025-09-08 18:38:07 +0200

    V_03

--- ejercicios-basicos-avanzados/ejercicio-12.js
+++ ejercicios-basicos-avanzados/ejercicio-12.js
@@ -1,22 +1,27 @@
-Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados.
-
+/* Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados.
 Pista: puedes generar un nuevo array y devolverlo.
+Puedes usar este array para probar tu función: */
 
-Puedes usar este array para probar tu función:
 
-const duplicates = [
-  'sushi',
-  'pizza',
-  'burger',
-  'potatoe',
-  'pasta',
-  'ice-cream',
-  'pizza',
-  'chicken',
-  'onion rings',
-  'pasta',
-  'soda'
-];
 function removeDuplicates(list) {
-  // Completar
+  return [...new Set(list)];
+ 
 }
+const duplicates = [
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda"
+];
+const sinDuplicates = removeDuplicates(duplicates);
+console.log(sinDuplicates);
+
+
+
--- ejercicios-basicos-avanzados/ejercicio-13.js
+++ ejercicios-basicos-avanzados/ejercicio-13.js
@@ -1,20 +1,22 @@
-Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
+/* Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
 
-Puedes usar este array para probar tu función:
+Puedes usar este array para probar tu función: */
 
-const names = [
-  'Peter',
-  'Steve',
-  'Tony',
-  'Natasha',
-  'Clint',
-  'Logan',
-  'Xabier',
-  'Bruce',
-  'Peggy',
-  'Jessica',
-  'Marc'
-];
-function nameFinder(nameList) {
-  // Completar
+
+function nameFinder(nameList, name) {
+  const find = nameList.includes(name);
+  const number = nameList.indexOf(name);
+
+    if (number !== -1){
+      return [find +" "+ number];
+    }
+    else {
+      return false;
+    }
+  
 }
+
+const names = [ 'Peter', 'Steve', 'Tony', 'Natasha', 'Clint', 'Logan', 'Xabier', 'Bruce', 'Peggy', 'Jessica', 'Marc'];
+
+console.log(nameFinder(names, 'Clint'));
+console.log(nameFinder(names, "Ro"));
