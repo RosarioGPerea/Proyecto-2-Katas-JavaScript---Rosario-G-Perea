9ea73a5e69789136eb0bb0fde2a6657e58a3892e
RosarioGPerea <rosario.g.perea@gmail.com>
2025-09-27 10:28:33 +0200

    V_33

--- ejercicios-basicos-avanzados/ejercicio-33.js
+++ ejercicios-basicos-avanzados/ejercicio-33.js
@@ -1,9 +1,9 @@
-Desarrolla una función que reciba un país por parámetro y devuelva su capital.
+/* Desarrolla una función que reciba un país por parámetro y devuelva su capital.
 
 Utiliza un objeto para almacenar los países y sus capitales.
 
 La función debe manejar casos en los que el país no esté en la lista, devolviendo un mensaje adecuado.
-
+ */
 const capitals = {
   Spain: 'Madrid',
   France: 'Paris',
@@ -18,5 +18,16 @@ const capitals = {
 };
 
 function getCapital(country) {
-  // insert code
-}
+  if (typeof country !== "string") {
+    return country;
+  }
+    const capital = capitals[country];
+    if (capital){
+      return ("La capital de " + country + " es " + capital);
+    } else {
+      return ("Esta capital -> " + country + " <- no está en la lista.");
+    }
+  }
+
+  console.log(getCapital("Greece"));
+  console.log(getCapital("Midgar"));
